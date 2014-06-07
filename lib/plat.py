b871e52df4612480e03f5c5b2e82900671cf5c34
guillermooo <guillermo.lopez@outlook.com>
2014-06-07 10:07:33 +0200

    Add Windows helper functions

--- lib/plat.py
+++ lib/plat.py
@@ -1,8 +1,23 @@
 import sublime
 
+import subprocess
+
 
 def is_windows():
     """Returns `True` if ST is running on Windows.
     """
     return sublime.platform() == 'windows'
 
+
+def supress_window():
+    """Returns a STARTUPINFO structure configured to supress windows.
+    Useful, for example, to supress console windows.
+
+    Works only on Windows.
+    """
+    if is_windows():
+        startupinfo = subprocess.STARTUPINFO()
+        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        startupinfo.wShowWindow = subprocess.SW_HIDE
+        return startupinfo
+    return None
