46fd9fa9805a6f0b0fca092d2caa4f934b187dbb
guillermooo <guillermo.lopez@outlook.com>
2014-06-16 21:58:02 +0200

    Add dumb Open Dart Editor command
    Adds a command to open the Dart Editor from ST. Ideally it should
    open the current file in the editor, and possibly at the same
    rowcol position, but I don't know if this is possible right now
    with the Dart Editor.
    
    Also adds support code:
    
    * Adds convenience function to build paths on Windows
    * Adds tests for said function
    * Adds OpenDartEditor plugin
    * Adds unrelated docstring and __init__ method
    * Updates .sublime-commands file
    * Adds SDK class that wraps the Dart sdk directory to provide
      convenient access to paths, tools, etc. (Basic implementation.)

--- misc.py
+++ misc.py
@@ -2,7 +2,25 @@ import sublime_plugin
 
 import webbrowser
 
+from .lib.sdk import SDK
+
 
 class OpenBrowser(sublime_plugin.WindowCommand):
+    """Opens API reference in default browser.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
     def run(self, url):
         webbrowser.open_new_tab(url)
+
+
+class OpenDartEditor(sublime_plugin.TextCommand):
+    """Opens the Dart Editor.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def run(self, edit):
+        sdk = SDK()
+        sdk.start_editor()
---
+++ tests/lib/test_plat.py
@@ -0,0 +1,29 @@
+import sublime
+
+import unittest
+import os
+
+from Dart.lib.plat import to_platform_path
+
+
+class Test_to_platform_path(unittest.TestCase):
+    def setUp(self):
+        self.view = sublime.active_window().new_file()
+
+    @unittest.skipUnless(os.name == 'nt', 'only for Windows')
+    def testAppendsExtensionOnWindows(self):
+        actual = to_platform_path('foo', '.exe')
+        self.assertEqual('foo.exe', actual)
+
+    @unittest.skipUnless(os.name == 'nt', 'only for Windows')
+    def testAppendsPathFragment(self):
+        actual = to_platform_path('foo', 'bar')
+        self.assertEqual('foo\\bar', actual)
+
+    @unittest.skipIf(os.name == 'nt', 'only for non-Windows platforms')
+    def testDoesNotAppendPathFragment(self):
+        actual = to_platform_path('foo', 'bar')
+        self.assertEqual('foo', actual)
+
+    def tearDown(self):
+        self.view.close()
