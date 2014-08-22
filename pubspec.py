eeff63b29f018e9ebd8b933bf34e22f3666df79c
guillermooo <guillermo.lopez@outlook.com>
2014-08-22 06:55:27 +0200

    Always set the pubspec syntax file

--- pubspec.py
+++ pubspec.py
@@ -5,9 +5,11 @@ import os
 
 
 PUBSPEC_TPL = '''# https://www.dartlang.org/tools/pub/pubspec.html
+
 name: ${1:your_package_name}
 version: ${2:0.1.0}
 '''
+
 
 class NewPubspecCommand(sublime_plugin.WindowCommand):
     '''Adds a pubspec file.
@@ -16,11 +18,10 @@ class NewPubspecCommand(sublime_plugin.WindowCommand):
         old_view = sublime.active_window().active_view()
 
         new_view = self.window.new_file()
+        new_view.settings().set('syntax', 'Packages/YAML/YAML.tmLanguage')
 
         if old_view and old_view.file_name():
-            new_view.settings().set('syntax', 'Packages/YAML/YAML.tmLanguage')
             new_view.settings().set('default_dir',
                                     os.path.dirname(old_view.file_name()))
 
         new_view.run_command('insert_snippet', {'contents': PUBSPEC_TPL})
-
