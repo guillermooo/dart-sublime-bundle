e1f0d06b658ea04400326f3dcfd445b1f9d2b337
guillermooo <guillermo.lopez@outlook.com>
2014-08-26 00:57:07 +0200

    Remove command to open default .dart settings
    We don't include those in the package any more, so it doesn't
    make sense to have it.
    
    - Remove command to open default .dart settings
    - Refactor

--- misc.py
+++ misc.py
@@ -11,7 +11,7 @@ from .lib.sdk import SDK
 _logger = PluginLogger(__name__)
 
 
-class OpenBrowser(sublime_plugin.WindowCommand):
+class OpenBrowserCommand(sublime_plugin.WindowCommand):
     """Opens API reference in default browser.
     """
     def __init__(self, *args, **kwargs):
@@ -21,7 +21,7 @@ class OpenBrowser(sublime_plugin.WindowCommand):
         webbrowser.open_new_tab(url)
 
 
-class OpenDartEditor(sublime_plugin.TextCommand):
+class OpenDartEditorCommand(sublime_plugin.TextCommand):
     """Opens the Dart Editor.
     """
     def __init__(self, *args, **kwargs):
@@ -32,14 +32,8 @@ class OpenDartEditor(sublime_plugin.TextCommand):
         sdk.start_editor()
 
 
-class OpenDartSettings(sublime_plugin.WindowCommand):
+class DartOpenSettingsCommand(sublime_plugin.WindowCommand):
     """Opens Dart settings files.
-
-    Default settings (that is, Packages/Dart/Support/Dart.sublime-settings)
-    are displayed as a read-only view not meant for editing.
-
-    User settings (that is, Packages/User/Dart.sublime-settings) are opened
-    as a regular view and are meant for editing.
     """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -49,39 +43,17 @@ class OpenDartSettings(sublime_plugin.WindowCommand):
         return os.path.join(sublime.packages_path(),
                             'User/Dart.sublime-settings')
 
-    def open_default(self):
-        """Prints the default settings for Dart to a read-only view. The user
-        should not edit their settings here, but use the 'user' file instead.
-        """
-        setts = sublime.load_resource(
-            'Packages/Dart/Support/Dart.sublime-settings')
-
-        v = self.window.new_file()
-        v.run_command('append', {"characters": setts.replace('\r', '')})
-        v.set_name('Dart Settings – Default (read-only)')
-        # TODO(guillermooo): ST should detect that this is a JSON file by
-        # looking at the extension, but it isn't the case. Check with
-        # Sublime HQ. For now, set the syntax manually.
-        v.set_syntax_file('Packages/JavaScript/JSON.tmLanguage')
-        v.set_scratch(True)
-        v.set_read_only(True)
-
     def run(self, kind='user'):
         """
         @kind: Any of (user, default).
         """
-        if kind == 'default':
-            _logger.debug('Opening default settings for viewing only.')
-            self.open_default()
-            return
-
         if kind != 'user':
             _logger.error('Unsupported settings type: %s', kind)
             return
 
         if not os.path.exists(self.user_file):
-            _logger.debug('Creating user settings file at: %s',
-                        self.user_file)
+            _logger.debug('creating user settings file at: %s',
+                          self.user_file)
             with open(self.user_file, 'w') as f:
                 f.write('{\n\t\n}')
 
