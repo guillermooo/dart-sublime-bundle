37406cebec14a2cff7cdfeaa25bc690e6671eb57
guillermooo <guillermo.lopez@outlook.com>
2014-08-26 08:45:32 +0200

    Reinstate command to check default settings
    - Refactor
    - Reinstate command to open default settings (global)
    - Small changes to make it obvious to users that they should not
      edit certain settings files.

--- misc.py
+++ misc.py
@@ -11,7 +11,9 @@ from .lib.sdk import SDK
 _logger = PluginLogger(__name__)
 
 
-class OpenBrowser(sublime_plugin.WindowCommand):
+# TODO(guillermooo): we probably don't need a new command; ST already includes
+# one by default.
+class DartOpenBrowserCommand(sublime_plugin.WindowCommand):
     """Opens API reference in default browser.
     """
     def __init__(self, *args, **kwargs):
@@ -21,7 +23,7 @@ class OpenBrowser(sublime_plugin.WindowCommand):
         webbrowser.open_new_tab(url)
 
 
-class OpenDartEditor(sublime_plugin.TextCommand):
+class DartOpenDartEditorCommand(sublime_plugin.TextCommand):
     """Opens the Dart Editor.
     """
     def __init__(self, *args, **kwargs):
@@ -32,20 +34,19 @@ class OpenDartEditor(sublime_plugin.TextCommand):
         sdk.start_editor()
 
 
-class OpenDartSettings(sublime_plugin.WindowCommand):
+class DartOpenSettingsCommand(sublime_plugin.WindowCommand):
     """Opens Dart settings files.
 
-    Default settings (that is, Packages/Dart/Support/Dart.sublime-settings)
-    are displayed as a read-only view not meant for editing.
+    - Default settings (that is, Packages/Dart/Support/Preferences.sublime-settings).
 
-    User settings (that is, Packages/User/Dart.sublime-settings) are opened
-    as a regular view and are meant for editing.
+    - Dart file settings (that is, Packages/User/Dart.sublime-settings)
+    are opened as a regular view and are meant for editing.
     """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     @property
-    def user_file(self):
+    def file_type_settings(self):
         return os.path.join(sublime.packages_path(),
                             'User/Dart.sublime-settings')
 
@@ -54,11 +55,11 @@ class OpenDartSettings(sublime_plugin.WindowCommand):
         should not edit their settings here, but use the 'user' file instead.
         """
         setts = sublime.load_resource(
-            'Packages/Dart/Support/Dart.sublime-settings')
+            'Packages/Dart/Support/Preferences.sublime-settings')
 
         v = self.window.new_file()
         v.run_command('append', {"characters": setts.replace('\r', '')})
-        v.set_name('Dart Settings – Default (read-only)')
+        v.set_name('Dart Settings - Default (read-only)')
         # TODO(guillermooo): ST should detect that this is a JSON file by
         # looking at the extension, but it isn't the case. Check with
         # Sublime HQ. For now, set the syntax manually.
@@ -79,16 +80,16 @@ class OpenDartSettings(sublime_plugin.WindowCommand):
             _logger.error('Unsupported settings type: %s', kind)
             return
 
-        if not os.path.exists(self.user_file):
+        if not os.path.exists(self.file_type_settings):
             _logger.debug('Creating user settings file at: %s',
-                        self.user_file)
-            with open(self.user_file, 'w') as f:
+                        self.file_type_settings)
+            with open(self.file_type_settings, 'w') as f:
                 f.write('{\n\t\n}')
 
         try:
-            self.window.open_file(self.user_file)
+            self.window.open_file(self.file_type_settings)
         except OSError as e:
             _logger.error('Unexpected error while trying to open %s',
-                self.user_file)
+                self.file_type_settings)
             _logger.error(e)
             _logger.error('=' * 80)
