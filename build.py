0809618eb76d2bb2e58239dbb68837cb864c5fbf
guillermooo <guillermo.lopez@outlook.com>
2014-09-02 23:32:16 +0200

    Implement smart build task
    - Add a context to detect a state where a launch is possible
    - Launch file/project
    
      If the active file is under 'web', start server and serve using
      Dartium.
    
      If the active file is under 'bin', run as a script.
    
    The new smart build feature overrides the default build systems
    only when the 'dart_can_do_launch' context is true. Otherwise,
    the pre-existing build tasks will be fired as usual.

--- build.py
+++ build.py
@@ -4,26 +4,28 @@
 import sublime
 import sublime_plugin
 
-import os
 from functools import partial
+import os
+import time
 
 from Dart import PluginLogger
-from Dart.lib.sdk import SDK
-from Dart.lib.dart_project import ViewInspector
-from Dart.lib.dart_project import find_pubspec
 from Dart.lib.build.base import DartBuildCommandBase
+from Dart.lib.dart_project import DartView
+from Dart.lib.dart_project import find_pubspec
+from Dart.lib.sdk import Dartium
+from Dart.lib.sdk import SDK
 
 
 _logger = PluginLogger(__name__)
 
 
 class ContextProvider(sublime_plugin.EventListener):
-    '''Implements the 'dart_is_project_file' context for .sublime-keymap
+    '''Implements the 'dart_can_do_launch' context for .sublime-keymap
     files.
     '''
     def on_query_context(self, view, key, operator, operand, match_all):
-        if key == 'dart_is_project_file':
-            return ViewInspector(view).is_project_file
+        if key == 'dart_can_do_launch':
+            return DartView(view).is_runnable
 
 
 class DartBuildProjectCommand(sublime_plugin.WindowCommand):
@@ -37,7 +39,7 @@ class DartBuildProjectCommand(sublime_plugin.WindowCommand):
           One of: 'primary', 'secondary'
         '''
         view = self.window.active_view()
-        if ViewInspector(view).is_pubspec:
+        if DartView(view).is_pubspec:
             self.window.run_command('dart_build_pubspec', {
                 'action': action,
                 'file_name': view.file_name()
@@ -57,41 +59,73 @@ class DartRunCommand(DartBuildCommandBase):
     '''
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.server = False
 
     def run(self, file_name, action='primary'):
         '''
         @action
           On of: primary, secondary
         '''
-        working_dir = find_pubspec(file_name)
-        if not working_dir:
-            working_dir = os.path.dirname(file_name)
+        try:
+            working_dir = os.path.dirname(find_pubspec(file_name))
+        except:
+            try:
+                if not working_dir:
+                    working_dir = os.path.dirname(file_name)
+            except:
+                _logger.debug('cannot run an unsaved file')
+                return
 
         sdk = SDK()
+        dart_view = DartView(self.window.active_view())
+
+        if dart_view.is_server_app:
+            self.run_server_app(file_name, working_dir)
+            return
+
+        if dart_view.is_web_app:
+            self.run_web_app(file_name, working_dir)
+            return
 
         if action == 'primary':
-            args = {
-                    'cmd' : [sdk.path_to_dart2js,
+            self.execute(
+                    cmd=[sdk.path_to_dart2js,
                                 '--minify', '-o', file_name + '.js',
                                 file_name],
-                    'working_dir': working_dir,
-                    'file_regex': "(\\S*):(\\d*):(\\d*): (.*)",
-                    'preamble': 'Running dart2js...\n',
-                    }
-            self.execute(**args)
+                    working_dir=working_dir,
+                    file_regex="(\\S*):(\\d*):(\\d*): (.*)",
+                    preamble='Running dart2js...\n',
+                    )
             return
 
         if action != 'secondary':
             _logger("unknown action: %s", action)
             return
 
-        args = {
-            'cmd': [sdk.path_to_dart, '--checked', file_name],
-            'working_dir': working_dir,
-            'file_regex': "'file:///(.+)': error: line (\\d+) pos (\\d+): (.*)$",
-            'preamble': 'Running dart...\n',
-        }
-        self.execute(**args)
+        self.run_server_app(file_name, working_dir)
+
+    def run_server_app(self, file_name, working_dir):
+        self.execute(
+            cmd=[SDK().path_to_dart, '--checked', file_name],
+            working_dir=working_dir,
+            file_regex="'file:///(.+)': error: line (\\d+) pos (\\d+): (.*)$",
+            preamble='Running dart...\n',
+            )
+
+    def run_web_app(self, file_name, working_dir):
+
+        if self.server:
+            self.execute(kill=True)
+
+        self.execute(
+            cmd=[SDK().path_to_pub, 'serve'],
+            working_dir=working_dir,
+            )
+        self.server = True
+
+        # TODO(guillermooo): run dartium in checked mode
+        sublime.set_timeout(lambda: Dartium().start('http:localhost:8080'),
+                            1000)
 
 
 class DartBuildPubspecCommand(DartBuildCommandBase):
@@ -118,12 +152,11 @@ class DartBuildPubspecCommand(DartBuildCommandBase):
         working_dir = os.path.dirname(file_name)
 
         if action == 'primary':
-            args = {
-                "cmd": [SDK().path_to_pub] + ['get'],
-                "working_dir": working_dir,
-                "preamble": "Running pub...\n",
-            }
-            self.execute(**args)
+            self.execute(
+                cmd=[SDK().path_to_pub] + ['get'],
+                working_dir=working_dir,
+                preamble="Running pub...\n",
+                )
             return
 
         if action != 'secondary':
@@ -137,9 +170,8 @@ class DartBuildPubspecCommand(DartBuildCommandBase):
         if idx == -1:
             return
 
-        args = {
-            'cmd': [SDK().path_to_pub] + [self.PUB_CMDS[idx]],
-            'working_dir': os.path.dirname(file_name),
-            "preamble": "Running pub...\n",
-        }
-        self.execute(**args)
+        self.execute(
+            cmd=[SDK().path_to_pub] + [self.PUB_CMDS[idx]],
+            working_dir=os.path.dirname(file_name),
+            preamble="Running pub...\n",
+            )
