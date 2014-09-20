72c3e22d8bd6e07dcc47850962b50afd434128e9
guillermooo <guillermo.lopez@outlook.com>
2014-09-20 11:05:55 +0200

    Add feedback

--- run.py
+++ run.py
@@ -310,6 +310,9 @@ class DartRunFileCommand(DartBuildCommandBase):
             DartRunFileCommand.observatory.stop()
             DartRunFileCommand.observatory = None
 
+        if self.panel:
+            self.panel.write('[Observatory stopped]')
+
     def on_data(self, text):
         self.panel.write(text)
 
