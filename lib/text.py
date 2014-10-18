7f90a245f607542315684fdb39e4ade65d7af71b
guillermooo <guillermo.lopez@outlook.com>
2014-10-18 09:18:23 +0200

    fix #373 - update license

--- lib/text.py
+++ lib/text.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2014, Guillermo López-Anglada. Please see the AUTHORS file for details.
+# All rights reserved. Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.)
+
 
 def decode_and_clean(data_bytes, encoding='utf-8'):
     return clean(decode(data_bytes, encoding))
