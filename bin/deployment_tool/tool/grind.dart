ee345e09e7cd6845e90bc54f14c7490cafe096be
guillermooo <guillermo.lopez@outlook.com>
2015-03-01 11:07:31 +0100

    use grind.dart for all tasks

--- bin/deployment_tool/tool/grind.dart
+++ bin/deployment_tool/tool/grind.dart
@@ -1,10 +1,36 @@
 import 'dart:io';
 
 import 'package:grinder/grinder.dart';
+import 'package:path/path.dart' as path;
 
-import '../bin/main.dart';
-import '../bin/perform_checks.dart';
+import '../bin/manifest.dart';
+import '../lib/environment.dart';
 
+
+// Helpers *******************************************************************
+/// Returns the path to the project's top-level directory.
+String get topLevel {
+  var scriptRoot = path.dirname(Platform.script.toFilePath());
+  return path.absolute(path.normalize(path.join(scriptRoot, '../../..')));
+}
+
+File copySync(File source, File destination) {
+  destination.createSync(recursive: true);
+  return source.copySync(destination.path);
+}
+
+String get loggerLevelFile {
+  var where = path.join(topLevel, 'sublime_plugin_lib/__init__.py');
+  return path.normalize(where);
+}
+
+bool checkLoggingLevelIsError() {
+  var file = new File(loggerLevelFile);
+  var regex = new RegExp(r'setLevel\(logging\.ERROR\)');
+  return regex.hasMatch(file.readAsStringSync());
+}
+
+// Task Definition ***********************************************************
 // TODO(guillermooo): use grinder "decorators".
 main([List<String> args]) {
   task('check', check);
@@ -13,6 +39,7 @@ main([List<String> args]) {
   startGrinder(args);
 }
 
+// Task Implementation *******************************************************
 void check(GrinderContext context) {
   if (!checkLoggingLevelIsError()) {
     var msg =
@@ -23,5 +50,25 @@ void check(GrinderContext context) {
 }
 
 void do_deploy(GrinderContext context) {
-  deploy();
+  var environment = new Environment(topLevel);
+
+  try {
+    var directory = new Directory(environment.destination);
+    if (directory.existsSync()) directory.deleteSync(recursive: true);
+    directory.createSync();
+  } catch (Exception) {
+    var message = 
+        'cannot delete/create destination directory: ${environment.destination}';
+    context.fail(message);
+  }
+
+  var manifest = new Manifest(included, environment);
+  manifest.list().where((entity) => entity is File).forEach((source) {
+    var relative = path.relative(source.path, from: environment.root);
+    var destination = new File(path.join(environment.destination, relative));
+    print('  ${source.path}');
+    copySync(source, destination);
+  });
+
+  return 0;
 }
