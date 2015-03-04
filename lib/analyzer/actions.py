4f2a56eebad6f7a21b4dc91fde95a8a40466d633
guillermooo <guillermo.lopez@outlook.com>
2015-03-04 07:49:38 +0100

    improve displaying of errors
    From now on, the results panel will be displayed when there are
    errors reported by the analysis server.
    
    F4/Shift+F4 will navigate through error instances. Error messages
    will be printed to the status bar and the results panel.

--- lib/analyzer/actions.py
+++ lib/analyzer/actions.py
@@ -95,10 +95,18 @@ def show_errors(errors):
 
     all_errs = set(errs_patts + warn_patts + info_patts)
 
+    if not all_errs:
+        panel = OutputPanel('dart.analyzer')
+        panel.hide()
+        return
+
     panel = OutputPanel('dart.analyzer')
     errors_pattern = r'^\w+\|\w+\|(.+)\|(\d+)\|(\d+)\|(.+)$'
     panel.set('result_file_regex', errors_pattern)
+    # This will overwrite any previous text.
     panel.write('\n'.join(all_errs))
+    panel.show()
+    sublime.status_message("Dart: Errors found")
 
 
 def clear_ui():
