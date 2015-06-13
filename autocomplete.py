9551dfb80711cb13d6b9db3d021a334ac8300212
guillermooo <guillermo.lopez@outlook.com>
2015-06-13 21:21:27 +0200

    tweak locations where completions are offered after "."
    - not in comments or strings
      In comments or strings, offer plain ST completions.

--- autocomplete.py
+++ autocomplete.py
@@ -7,11 +7,11 @@ from Dart.sublime_plugin_lib import PluginLogger
 from Dart.sublime_plugin_lib.events import IdleIntervalEventListener
 from Dart.sublime_plugin_lib.path import is_active
 
-
 from Dart import analyzer
-from Dart.analyzer import AnalysisServer
 from Dart import editor_context
+from Dart.analyzer import AnalysisServer
 from Dart.lib.path import is_view_dart_script
+from Dart.lib.pub_package import DartFile
 
 
 _logger = PluginLogger(__name__)
@@ -20,14 +20,26 @@ _logger = PluginLogger(__name__)
 class IdleAutocomplete(IdleIntervalEventListener):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.duration = 400
+        self.duration = 300
 
     def check(self, view):
-        return True
+        # Offer Dart completions in Dart files when the caret isn't in a
+        # string or comment. If strings or comments, offer plain Sublime Text
+        # completions.
+        return (not self._in_string_or_comment(view)
+                and DartFile(view).is_dart_file)
 
     def on_idle(self, view):
-        prev_char = view.substr(view.sel()[0].begin() - 1)
-        if not prev_char in '.':
+        self._show_completions(view)
+
+    def _show_completions(self, view):
+        try:
+            # TODO: We probably should show completions after other chars.
+            is_after_dot = view.substr(view.sel()[0].b - 1) == '.'
+        except IndexError:
+            return
+
+        if not is_after_dot:
             return
 
         if not AnalysisServer.ping():
@@ -39,7 +51,13 @@ class IdleAutocomplete(IdleIntervalEventListener):
 
         if is_active(view):
             view.window().run_command('dart_get_completions')
-            # view.window().run_command('hide_auto_complete')
+
+    def _in_string_or_comment(self, view):
+        try:
+            return view.match_selector(view.sel()[0].b,
+                    'source.dart string, source.dart comment')
+        except IndexError:
+            pass
 
 
 class DartGetCompletions(sublime_plugin.WindowCommand):
@@ -58,19 +76,23 @@ class DartGetCompletions(sublime_plugin.WindowCommand):
 
 
 class AutocompleteEventListener(sublime_plugin.EventListener):
-    _PROPERTY = '\u25CB {}'
-    _FUNCTION = '\u25BA {}'
     _INHIBIT_OTHER = (sublime.INHIBIT_WORD_COMPLETIONS |
             sublime.INHIBIT_EXPLICIT_COMPLETIONS)
 
-    def _format(self, completions):
-        pass
+    @staticmethod
+    def _in_string_or_comment(view, locations):
+        return all((view.match_selector(loc, 'source.dart comment, source.dart string')
+            or view.match_selector(loc - 1, 'source.dart comment, sorce.dart string'))
+                for loc in locations)
 
     def on_query_completions(self, view, prefix, locations):
         if not is_view_dart_script(view):
             return ([], 0)
 
         if view.settings().get('command_mode') is True:
+            return ([], self._INHIBIT_OTHER)
+
+        if self._in_string_or_comment(view, locations):
             return ([], 0)
 
         completions = []
@@ -81,4 +103,4 @@ class AutocompleteEventListener(sublime_plugin.EventListener):
         if completions:
             return (completions, self._INHIBIT_OTHER)
 
-        return ([], 0)
+        return ([], self._INHIBIT_OTHER)
