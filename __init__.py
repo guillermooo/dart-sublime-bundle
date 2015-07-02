240e2ba26b652d943f929685c589def22cfe0736
guillermooo <guillermo.lopez@outlook.com>
2015-07-02 19:21:36 +0200

    add logger to __init__

--- __init__.py
+++ __init__.py
@@ -1,4 +1,11 @@
+from Dart.sublime_plugin_lib import PluginLogger
+
 from Dart.lib.editor_context import EditorContext
+
+import logging
+
+
+_logger = PluginLogger(__name__)
 
 
 editor_context = EditorContext()
